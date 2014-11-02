98bf7d28364d8bad05436540712b5730fc2172cc
ron <ron.kirschler@gmail.com>
2014-11-02 01:50:54 +0100

    Initial commit

---
+++ src/Extenders/ContextExtender.php
@@ -0,0 +1,35 @@
+<?php
+namespace extenders;
+
+use Logger\Common\AbstractLoggerAware;
+use Psr\Log\LoggerInterface;
+
+class ContextExtender extends AbstractLoggerAware {
+	/**
+	 * @var array
+	 */
+	private $keyValueArray;
+
+	/**
+	 * @param LoggerInterface $logger
+	 * @param array $keyValueArray
+	 */
+	public function __construct(LoggerInterface $logger, array $keyValueArray) {
+		parent::__construct($logger);
+		$this->keyValueArray = $keyValueArray;
+	}
+
+	/**
+	 * Logs with an arbitrary level.
+	 * @param string $level
+	 * @param string $message
+	 * @param array $context
+	 * @return void
+	 */
+	public function log($level, $message, array $context = array()) {
+		foreach($this->keyValueArray as $key => $value) {
+			$context[$key] = (string) $value;
+		}
+		$this->logger()->log($level, $message, $context);
+	}
+}

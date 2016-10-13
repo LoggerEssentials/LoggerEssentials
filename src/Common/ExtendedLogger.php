70118ae6f0f28becf7f2da1dacc9c81089bedbc9
Ron Kirschler <ron.kirschler@gmail.com>
2016-10-13 12:02:38 -0400

    - Added `ExtendedLogger::intercept`

--- src/Common/ExtendedLogger.php
+++ src/Common/ExtendedLogger.php
@@ -1,6 +1,7 @@
 <?php
 namespace Logger\Common;
 
+use Exception;
 use Psr\Log\LoggerInterface;
 
 interface ExtendedLogger extends LoggerInterface {
@@ -17,4 +18,12 @@ interface ExtendedLogger extends LoggerInterface {
 	 * @return ExtendedLogger
 	 */
 	public function context($caption, array $context = array(), $fn);
+	
+	/**
+	 * @param callable $fn
+	 * @param callable $callback
+	 * @return mixed
+	 * @throws Exception
+	 */
+	public function intercept($fn, $callback);
 }

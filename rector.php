e4d7cc70333b8aa0b3ecba7e7a504897b36bbc15
rkr <ron.kirschler@gmail.com>
2025-11-22 19:59:58 +0100

    Cleaned up `rector.php`: removed outdated comment and adjusted indentation.

--- rector.php
+++ rector.php
@@ -9,8 +9,7 @@ return RectorConfig::configure()
         __DIR__ . '/src',
         __DIR__ . '/tests',
     ])
-    // uncomment to reach your current PHP version
-     ->withPhpSets()
+	->withPhpSets()
     ->withTypeCoverageLevel(0)
     ->withDeadCodeLevel(0)
     ->withCodeQualityLevel(0);
